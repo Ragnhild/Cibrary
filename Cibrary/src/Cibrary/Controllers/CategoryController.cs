4ea61b48c4e2c0226588cac72f298be24c2b621d
Ragnhild Kosmo Holm <ragnhild.kosmo.holm@Ciber.com>
2015-09-16 13:24:50 +0200

    can create new category - edit old category (and category for books is updated) - can delete category

---
+++ Cibrary/src/Cibrary/Controllers/CategoryController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cibrary.Models;
+using Microsoft.AspNet.Mvc;
+using Microsoft.Data.Entity;
+
+namespace Cibrary.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            IEnumerable<Category> category = _db.Categories.ToList();
+            return View(category);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            _db.Categories.Add(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Category category = _db.Categories.SingleOrDefault(x => x.Id == id);
+            return View(category);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Category category)
+        {
+            _db.Categories.Attach(category);
+            _db.Entry(category).State = EntityState.Modified;
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Category category = _db.Categories.SingleOrDefault(x => x.Id == id);
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+    }
+}
